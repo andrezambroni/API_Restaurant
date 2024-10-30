52978eab9d6bb7437dc748ff51c40d8401ab1c34
andrezambroni <andrezambroni@gmail.com>
2024-10-30 16:36:26 -0300

    feat: configurar servidor Express com rotas e middleware de tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import { routes } from "./routes"
+import { errorHandling } from "./middlewares/error-handling"
 
 const PORT = 3333
 
@@ -7,5 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use(errorHandling)
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
