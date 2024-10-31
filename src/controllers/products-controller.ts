bc9cec070a118992bba36593bd548216490334fe
andrezambroni <andrezambroni@gmail.com>
2024-10-31 18:45:00 -0300

    feat: adicionado funcionalidade de criação de produtos

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express"
+import  { knex } from "@/database/knex"
+import { z } from "zod"
 
 export class ProductsController {
   // pq ta usando async
@@ -9,5 +11,21 @@ export class ProductsController {
       next(error)
     }
   }
-}
 
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0),
+      })
+
+      const { name, price } = bodySchema.parse(request.body)
+
+      await knex<ProductRepository>("products").insert({ name, price })
+
+      return response.status(201).json()
+    } catch (error) {
+      next(error)
+    }
+  }
+}
