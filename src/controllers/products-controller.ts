8eac8045af65038fd7db742e71e710d2216bf0f1
andrezambroni <andrezambroni@gmail.com>
2024-11-04 16:04:02 -0300

    feat: adicionado validação e inserção de produtos no controlador

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,12 +1,19 @@
 import { NextFunction, Request, Response } from "express"
-import  { knex } from "@/database/knex"
+import { knex } from "@/database/knex"
 import { z } from "zod"
 
 export class ProductsController {
   // pq ta usando async
   async index(request: Request, response: Response, next: NextFunction) {
     try {
-      return response.json({ message: "OK" })
+      const { name } = request.query
+
+      const products = await knex<ProductRepository>("products")
+        .select()
+        .whereLike("name", `%${name ?? ""}%`)
+        .orderBy("name")
+
+      return response.json(products)
     } catch (error) {
       next(error)
     }
@@ -28,4 +35,49 @@ export class ProductsController {
       next(error)
     }
   }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "id must be a number" })
+        .parse(request.params.id)
+
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0),
+      })
+
+      const { name, price } = bodySchema.parse(request.body)
+
+      await knex<ProductRepository>("products")
+        .update({ name, price, updated_at: knex.fn.now() })
+        .where({ id })
+
+      return response.json()
+    } catch (error) {
+      next(error)
+    }
+  }
 }
+
+/**
+ * async: Indica que o método é assíncrono, permitindo o uso de await dentro dele. 
+ * Isso é útil para operações que podem demorar, como consultas ao banco de dados
+ * 
+ * next: É uma função que passa o controle para o próximo middleware em caso de erro
+ * 
+ * A validação é feita usando a biblioteca Zod (z), e o knex é usado para interagir com o banco de dados. 
+ * Em caso de erro, o next é chamado para passar o controle para o próximo middleware de erro.
+ * 
+ * parse(request.body): O método parse do Zod valida os dados do corpo da requisição (request.body) de acordo com o esquema definido em bodySchema. 
+ *  Se os dados não estiverem de acordo com o esquema, uma exceção será lançada.
+ * 
+ * bodySchema: É um esquema de validação definido usando a biblioteca Zod. 
+ *  Este esquema especifica as regras que os dados do corpo da requisição (request.body) devem seguir.
+ * 
+ * knex<ProductRepository>("products"): knex é um query builder para SQL. Aqui, ele está sendo configurado para operar na tabela "products". 
+ *  O tipo ProductRepository é usado para tipar os dados que serão manipulados
+ * 
+ */
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -1,10 +1,43 @@
 import { Router } from "express"
-import {ProductsController} from "@/controllers/products-controller"
+import { ProductsController } from "@/controllers/products-controller"
 
 const productsRoutes = Router()
 const productsController = new ProductsController()
 
 productsRoutes.get("/", productsController.index)
 productsRoutes.post("/", productsController.create)
+productsRoutes.put("/:id", productsController.update)
 
 export { productsRoutes }
+
+/**
+ este arquivo configura e exporta um conjunto de rotas para operações CRUD (Create, Read, Update) relacionadas a produtos, 
+ utilizando um controlador específico para lidar com a lógica dessas operações
+
+
+ Router() é uma função do Express que cria um novo objeto roteador. 
+    Este objeto roteador é usado para definir rotas de forma modular e montável.
+
+const productsRoutes = Router()
+Ao criar productsRoutes, você está criando um conjunto de rotas específicas para o recurso de produtos. 
+    Isso ajuda a organizar o código, mantendo as rotas relacionadas a produtos separadas de outras rotas.
+
+const productsController = new ProductsController()
+ProductsController é uma classe que contém métodos para manipular operações relacionadas a produtos, como listar, criar e atualizar produtos.
+    Ao criar uma instância de ProductsController, você está preparando um objeto que pode ser usado para chamar esses métodos. 
+    Isso permite que você separe a lógica de negócios (no controlador) da definição das rotas (no roteador).
+   
+   
+   ProductsController é uma classe que define métodos para manipular operações relacionadas a produtos, como listar, criar e atualizar produtos.
+   Quando você escreve new ProductsController(), você está criando uma nova instância da classe ProductsController.
+    Isso significa que você está criando um novo objeto que tem todas as propriedades e métodos definidos na classe ProductsController.
+
+A nova instância criada é atribuída à constante productsController.
+ Agora, productsController é um objeto que pode ser usado para chamar os métodos definidos na classe ProductsController.
+
+Por que Instanciar?
+Instanciar uma classe permite que você crie objetos que podem ser usados para realizar tarefas específicas.
+ No seu caso, a instância productsController é usada para manipular operações relacionadas a produtos nas rotas definidas.
+
+
+    */
