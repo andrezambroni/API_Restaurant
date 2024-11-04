782172d22f3a818152b5501b68b9204c98e3185b
andrezambroni <andrezambroni@gmail.com>
2024-11-04 16:55:50 -0300

    feat: adicionado seed de produtos no banco de dados e metodo remove

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -2,8 +2,9 @@ import { NextFunction, Request, Response } from "express"
 import { knex } from "@/database/knex"
 import { z } from "zod"
 
+import { AppError } from "@/utils/AppError"
+
 export class ProductsController {
-  // pq ta usando async
   async index(request: Request, response: Response, next: NextFunction) {
     try {
       const { name } = request.query
@@ -51,6 +52,15 @@ export class ProductsController {
 
       const { name, price } = bodySchema.parse(request.body)
 
+      const product = await knex<ProductRepository>("products")
+        .select()
+        .where({ id })
+        .first()
+
+        if(!product){
+          throw new AppError("product not found")
+        }
+
       await knex<ProductRepository>("products")
         .update({ name, price, updated_at: knex.fn.now() })
         .where({ id })
@@ -60,24 +70,49 @@ export class ProductsController {
       next(error)
     }
   }
+
+  async remove(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "id must be a number" })
+        .parse(request.params.id)
+
+      const product = await knex<ProductRepository>("products")
+        .select()
+        .where({ id })
+        .first()
+
+      if (!product) {
+        throw new Error("product not found")
+      }
+
+      await knex<ProductRepository>("products").delete().where({ id })
+
+      return response.json()
+    } catch (error) {
+      next(error)
+    }
+  }
 }
 
 /**
- * async: Indica que o método é assíncrono, permitindo o uso de await dentro dele. 
+ * async: Indica que o método é assíncrono, permitindo o uso de await dentro dele.
  * Isso é útil para operações que podem demorar, como consultas ao banco de dados
- * 
+ *
  * next: É uma função que passa o controle para o próximo middleware em caso de erro
- * 
- * A validação é feita usando a biblioteca Zod (z), e o knex é usado para interagir com o banco de dados. 
+ *
+ * A validação é feita usando a biblioteca Zod (z), e o knex é usado para interagir com o banco de dados.
  * Em caso de erro, o next é chamado para passar o controle para o próximo middleware de erro.
- * 
- * parse(request.body): O método parse do Zod valida os dados do corpo da requisição (request.body) de acordo com o esquema definido em bodySchema. 
+ *
+ * parse(request.body): O método parse do Zod valida os dados do corpo da requisição (request.body) de acordo com o esquema definido em bodySchema.
  *  Se os dados não estiverem de acordo com o esquema, uma exceção será lançada.
- * 
- * bodySchema: É um esquema de validação definido usando a biblioteca Zod. 
+ *
+ * bodySchema: É um esquema de validação definido usando a biblioteca Zod.
  *  Este esquema especifica as regras que os dados do corpo da requisição (request.body) devem seguir.
- * 
- * knex<ProductRepository>("products"): knex é um query builder para SQL. Aqui, ele está sendo configurado para operar na tabela "products". 
+ *
+ * knex<ProductRepository>("products"): knex é um query builder para SQL. Aqui, ele está sendo configurado para operar na tabela "products".
  *  O tipo ProductRepository é usado para tipar os dados que serão manipulados
- * 
+ *
  */
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -7,7 +7,7 @@ const productsController = new ProductsController()
 productsRoutes.get("/", productsController.index)
 productsRoutes.post("/", productsController.create)
 productsRoutes.put("/:id", productsController.update)
-
+productsRoutes.delete("/:id", productsController.remove)
 export { productsRoutes }
 
 /**
