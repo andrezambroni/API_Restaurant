c60034b97d69593438c9164007ca684b95dc235e
andrezambroni <andrezambroni@gmail.com>
2024-11-05 17:07:44 -0300

    feat: abertura e fechamento de mesa

--- src/controllers/tables-sessions.controller.ts
+++ src/controllers/tables-sessions.controller.ts
@@ -1,9 +1,74 @@
 import { Request, Response, NextFunction } from "express"
+import { knex } from "@/database/knex"
+import { z } from "zod"
+import { AppError } from "@/utils/AppError"
 
 export class TableSessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
-      response.status(201).json({ message: "Table session created" })
+      const bodySchema = z.object({
+        table_id: z.number(),
+      })
+
+      const { table_id } = bodySchema.parse(request.body)
+
+      const session = await knex<TablesSessionsRepository>("table_sessions")
+        .where({ table_id })
+        .orderBy("opened_at", "desc")
+        .first()
+
+      if (session && !session.closed_at) {
+        throw new AppError("Table already has an open session")
+      }
+
+      await knex<TablesSessionsRepository>("table_sessions").insert({
+        table_id,
+        opened_at: knex.fn.now(),
+      })
+
+      response.status(201).json()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      const sessions = await knex<TablesSessionsRepository>(
+        "table_sessions"
+      ).orderBy("closed_at")
+
+      response.json(sessions)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "id must be a number" })
+        .parse(request.params.id)
+
+      const session = await knex<TablesSessionsRepository>("table_sessions")
+        .where({ id })
+        .first()
+
+      if (!session) {
+        throw new AppError("Session table not found")
+      }
+
+      if (session.closed_at) {
+        throw new AppError("Session table already closed")
+      }
+
+      await knex<TablesSessionsRepository>("table_sessions")
+        .update({ closed_at: knex.fn.now() })
+        .where({ id })
+
+      return response.json()
     } catch (error) {
       next(error)
     }
--- src/routes/tables-sessions-routes.ts
+++ src/routes/tables-sessions-routes.ts
@@ -6,5 +6,12 @@ const tableSessionsRoutes = Router()
 const tableSessionsController = new TableSessionsController()
 
 tableSessionsRoutes.post("/", tableSessionsController.create)
-
+tableSessionsRoutes.get("/", tableSessionsController.index)
+tableSessionsRoutes.patch("/:id", tableSessionsController.update) // Atualização parcial
+// Se tivesse uma rota PUT, seria para atualização completa
 export { tableSessionsRoutes }
+
+/*
+PUT: Substitui completamente o recurso. Usado para atualizações completas.
+PATCH: Modifica parcialmente o recurso. Usado para atualizações parciais.
+*/
