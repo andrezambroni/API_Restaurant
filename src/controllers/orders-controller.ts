dfb1459058223a41cd307ee8d5bd54392e8eb9fc
andrezambroni <andrezambroni@gmail.com>
2024-11-06 16:27:10 -0300

    feat: salvando pedido, listar pedido por mesa e regras de negocio

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express"
 import { knex } from "@/database/knex"
 import { z } from "zod"
+import { AppError } from "@/utils/AppError"
 
 export class OrdersController {
   async create(request: Request, response: Response, next: NextFunction) {
@@ -15,7 +16,45 @@ export class OrdersController {
         request.body
       )
 
+      const session = await knex<TablesSessionsRepository>("table_sessions")
+        .where({ id: table_session_id })
+        .first()
+
+      if (!session) {
+        throw new AppError("Table session not found")
+      }
+
+      if (session.closed_at) {
+        throw new AppError("Table session is closed")
+      }
+
+      const product = await knex<ProductRepository>("products")
+        .where({ id: product_id })
+        .first()
+
+      if (!product) {
+        throw new AppError("Product not found")
+      }
+
+      await knex<OrderRepository>("orders").insert({
+        table_session_id,
+        product_id,
+        quantity,
+        price: product.price,
+      })
+
       response.status(201).json()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+        const { table_session_id } = request.params
+
+        const order = await knex("orders").where({ table_session_id })
+      return response.json(order)
     } catch (error) {
       next(error)
     }
